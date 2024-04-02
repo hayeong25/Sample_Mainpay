5558e81affb916a105147535426cba702b929527
hayeong25 <hayeong0624@gmail.com>
2024-04-02 17:00:11 +0900

    init

---
+++ src/main/java/com/sample/mainpay/MainpayApplication.java
@@ -0,0 +1,13 @@
+package com.sample.mainpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainpayApplication.class, args);
+	}
+
+}
